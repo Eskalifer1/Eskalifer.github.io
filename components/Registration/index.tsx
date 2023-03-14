c32a4f27076fcd3127072e2fe73ffecc305ed618
Artem Achkasov <artemkryt1800@gmail.com>
2023-03-14 17:11:26 +0200

    register page fix

--- components/Registration/index.tsx
+++ components/Registration/index.tsx
@@ -1,14 +1,18 @@
 import dynamic from "next/dynamic";
 import React from "react";
-import registrationImage from "./../../assets/images/registration.jpg"
+import registrationImage from "./../../assets/images/registration.jpg";
 
-const HalfPageImage = dynamic(() => import('components/Images/HalfPageImage'))
-const RegistrationForm = dynamic(() => import('./RegistrationForm'), {ssr: false})
+const HalfPageImage = dynamic(() => import("components/Images/HalfPageImage"));
+const RegistrationForm = dynamic(() => import("./RegistrationForm"), {
+  ssr: false,
+});
 
 const Registration = () => {
   return (
     <div className="wrap flex">
-      <RegistrationForm />
+      <div className="minmd:min-w-[50%] min-w-[100%]">
+        <RegistrationForm />
+      </div>
       <HalfPageImage source={registrationImage} alt={"Registration Image"} />
     </div>
   );
